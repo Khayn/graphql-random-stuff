c88d980a0c320342213bf96fe82f372c891417a9
Mateusz Garczyński <evil.khai@gmail.com>
2021-08-19 01:04:21 +0200

    added greeting and interetingUrls endpoints

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = `type Query {
-    greeting: String
-}`;
-const server = new ApolloServer({ typeDefs });
+const typeDefs = gql`
+    type Query {
+      greeting: String,
+      interestingUrls: [String]
+    }
+`;
+
+const data = {
+    greeting: "Hello!",
+    interestingUrls : ["www.wsieci.pl", "www.dorzeczy.pl"]
+
+}
+const server = new ApolloServer({ typeDefs, rootValue : data });
 
 server.listen({ port: 4000 }).then(result => console.log(result.url));
