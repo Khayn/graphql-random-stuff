27bc65492bed0914b0f3765c581a161f6827b906
Mateusz Garczyński <evil.khai@gmail.com>
2021-08-19 00:49:39 +0200

    simple GraphQL server

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello world!")
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = `type Query {
+    greeting: String
+}`;
+const server = new ApolloServer({ typeDefs });
+
+server.listen({ port: 4000 }).then(result => console.log(result.url));
