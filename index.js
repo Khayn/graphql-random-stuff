c7a1705fe909626bea7b7a629158987094e6ecd2
Mateusz Garczyński <evil.khai@gmail.com>
2021-08-29 21:41:46 +0200

    fixed formatting

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const { ApolloServer, gql } = require("apollo-server");
+const {
+	ApolloServer,
+	gql
+} = require("apollo-server");
 
-const typeDefs = gql`
-    type Query {
-      greeting: String,
-      interestingUrls: [String]
-    }
+const typeDefs = gql `
+	type Query {
+	  greeting: String,
+	  interestingUrls: [String]
+	}
 `;
 
 const data = {
-    greeting: "Hello!",
-    interestingUrls : ["www.wsieci.pl", "www.dorzeczy.pl"]
+	greeting: "Hello!",
+	interestingUrls: ["www.wsieci.pl", "www.dorzeczy.pl"]
 
 }
-const server = new ApolloServer({ typeDefs, rootValue : data });
+const server = new ApolloServer({
+	typeDefs,
+	rootValue: data
+});
 
-server.listen({ port: 4000 }).then(result => console.log(result.url));
+server.listen({
+	port: 4000
+}).then(result => console.log(result.url));
